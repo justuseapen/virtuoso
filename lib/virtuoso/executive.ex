ba11326e2ebb6ddcde0cd49816f8d33c27ff8fca
Justus Eapen <justuseapen@gmail.com>
2018-06-23 16:19:57 -0400

    Fixes warnings

--- lib/virtuoso/executive.ex
+++ lib/virtuoso/executive.ex
@@ -3,7 +3,6 @@ defmodule Virtuoso.Executive do
   Receives impressions and delegates to relevant bot and subroutine
   """
 
-  alias Virtuoso.Ego
   alias Virtuoso.Bot
 
   def handles_message({%{recipient_id: recipient_id} = impression, conversation_state}) do
@@ -12,14 +11,10 @@ defmodule Virtuoso.Executive do
     |> get_subroutine(impression, conversation_state)
   end
 
-  @doc """
-  Matches recipient_id to corresponding bot
-  """
+  # Matches recipient_id to corresponding bot
   defp identify(receiver_id), do: Virtuoso.Bot.get(receiver_id)
 
-  @doc """
-  Gets the relevant subroutine from the relevant bot
-  """
+  # Gets the relevant subroutine from the relevant bot
   defp get_subroutine(bot, impression, conversation_state) do
     bot
     |> Bot.handles_message(impression, conversation_state)
