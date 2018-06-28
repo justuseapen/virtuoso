84abeb3c5ca3a67a56671fc206878c777f6bd0ec
Justus Eapen <justuseapen@gmail.com>
2018-06-28 09:30:02 -0400

    adds hex package config

--- mix.exs
+++ mix.exs
@@ -10,6 +10,8 @@ defmodule Virtuoso.Mixfile do
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
       start_permanent: Mix.env == :prod,
       aliases: aliases(),
+      description: description(),
+      package: package(),
       deps: deps()
     ]
   end
@@ -39,6 +41,7 @@ defmodule Virtuoso.Mixfile do
   defp deps do
     [
       {:cowboy, "~> 1.0"},
+      {:ex_doc, ">= 0.0.0", only: :dev},
       {:gettext, "~> 0.11"},
       {:httpoison, "~> 0.13"},
       {:phoenix, "~> 1.3.0"},
@@ -62,6 +65,21 @@ defmodule Virtuoso.Mixfile do
       "ecto.setup": ["ecto.create", "ecto.migrate", "run priv/repo/seeds.exs"],
       "ecto.reset": ["ecto.drop", "ecto.setup"],
       "test": ["ecto.create --quiet", "ecto.migrate", "test"]
+    ]
+  end
+
+  defp description do
+      """
+      Phoenix-based framework for chatbot development and orchestration.
+      """
+  end
+
+  defp package do
+    [
+      files: ["lib", "mix.exs", "README*"],
+      maintainers: ["Justus Eapen"],
+      licenses: ["MIT"],
+      links: %{"GitHub" => "https://github.com/justuseapen/virtuoso"}
     ]
   end
 end
