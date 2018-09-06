363dacbaff1318bad6fa42a1eec440cb6d03776b
Justus Eapen <justuseapen@gmail.com>
2018-09-05 20:31:37 -0700

    Versionbump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Virtuoso.Mixfile do
   def project do
     [
       app: :virtuoso,
-      version: "0.0.23",
+      version: "0.0.24",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env),
       compilers: [:phoenix, :gettext] ++ Mix.compilers,
