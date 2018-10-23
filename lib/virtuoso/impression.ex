105863c5028b870b6ee57bf1f7b65cc26ed3240b
Justus Eapen <justuseapen@gmail.com>
2018-10-23 09:32:08 -0400

    Adds impression module and struct

--- lib/virtuoso/impression.ex
+++ lib/virtuoso/impression.ex
@@ -3,6 +3,14 @@ defmodule Virtuoso.Impression do
   Internal representation of incoming messages
   """
   @enforce_keys [:sender_id, :recipient_id]
-  defstruct message: nil, url: nil, sender_id: :init, recipient_id: :init, timestamp: :timestamp
-end
+  defstruct [:message, :url, :sender_id, :recipient_id, :timestamp, :intent]
 
+  @doc "Creates an impression of an experience to be understood and responded to"
+  def build(args) do
+    %__MODULE__{
+      message: args[:message],
+      sender_id: args[:sender_id],
+      recipient_id: args[:recipient_id]
+    }
+  end
+end
