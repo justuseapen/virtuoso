8dcb7f659985c77498be39a9e7b640b604ac997e
Justus Eapen <justuseapen@gmail.com>
2019-03-19 15:24:14 -0400

    Adds function doc to fast thinking

--- lib/memento_mori/fast_thinking.ex
+++ lib/memento_mori/fast_thinking.ex
@@ -4,6 +4,15 @@ defmodule MementoMori.FastThinking do
   This allows us to bypass SlowThinking when it is performant to do so.
   """
 
+  @doc """
+  Add new function definitions, pattern matching against the impression in order to catch common user intents.
+
+  def run(%{message: "Hello Bot"} = impression) do
+    impression
+    |> Map.merge(%{intent: "hello_world"})
+  end
+  """
+
   def run(%{message: "25"} = impression) do
     impression
     |> Map.merge(%{intent: "time_left"})
